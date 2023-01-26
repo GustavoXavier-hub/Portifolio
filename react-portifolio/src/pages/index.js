6cfd15955031e5f482345aeff0bb9ada1fdd9b6f
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2023-01-26 16:58:04 -0300

    Config-projeto

---
+++ react-portifolio/src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+   <div>
+   <head>
+   <title>Gustavo Xavier Portifolio</title>  
+   <meta name="description" content="generate by react"></meta>
+   <link rel="icon" href="/react-portifolio/public/favicon.ico"></link>
+   </head>
+
+   <main></main>
+   </div>               
+  );
+}
