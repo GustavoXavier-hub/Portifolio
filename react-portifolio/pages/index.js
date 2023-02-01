f811881cb625ff5ac20a09f535737b493192902a
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2023-02-01 20:20:08 -0300

    estrutura da pagina

--- react-portifolio/pages/index.js
+++ react-portifolio/pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
-//import { BsFillMoonStarsFill } from "react-icons/bs";
-
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
 export default function Home() {
   return (
     <div>
@@ -16,15 +20,30 @@ export default function Home() {
         <nav className="py-10 mb-12 flex justify-between">
        <h1 className="text-xl">developedbyed</h1>
         <ul className="flex  items-center">
-          {/* <li><BsFillMoonStarsFill className="cursor-pointer text-2xl"/></li> */}
-          <li><a className="bg-cyan-500 text-white px-4 py-2 rounded-md ml-8" href="#">
-            Resumo
+           <li><BsFillMoonStarsFill className="cursor-pointer text-2xl"/></li> 
+          <li>
+            <a className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+          href="#">
+            Resume
             </a>
             </li>
         </ul>
         </nav>
+        <div>
+        <h2>Gustavo Xavier</h2>
+        <h3>Developer Back-end.</h3>
+        <p>
+          Progamador fazendo sistemas e aplicaçoes web mobile desktop
+        </p>
+        </div>
+        <div>
+          <AiFillTwitterCircle/>
+          <AiFillLinkedin/>
+          <AiFillYoutube/>
+       </div>
+       <div></div>
       </section>
       </main>  
     </div>
-  )
+  );
 }
